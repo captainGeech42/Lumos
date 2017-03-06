9cb970906742fd4f8ec991e6790272fd3940d165
Zander Work <zander@zanderwork.com>
2017-03-06 12:27:45 -0800

    made new character superclass to replace interface

--- src/com/lumos/Character.java
+++ src/com/lumos/Character.java
@@ -1,10 +1,84 @@
 package com.lumos;
 
-public interface Character {
+public class Character {
+	private int x;
+	private int y;
+	private int dx;
+	private int dy;
 	
-	public int getHealth();
-	public void setHealth(int health);
+	private int health;
+	private int strength;
+
+	private String name;
 	
-	public String getName();
-	public void setName(String name);
+	protected Character(int health, String name) {
+		this.health = health;
+		this.name = name;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;		
+	}
+	
+	public int getStrength() {
+		return strength;
+	}
+	
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getDx() {
+		return dx;
+	}
+	
+	public void setDx(int dx) {
+		this.dx = dx;
+	}
+	
+	public int getDy() {
+		return dy;
+	}
+	
+	public void setDy(int dy) {
+		this.dy = dy;
+	}
+	
+	public void moveX() {
+		x += dx;
+		dx = 0;
+	}
+	
+	public void moveY() {
+		y += dy;
+		dy = 0;
+	}
 }
--- src/com/lumos/Player.java
+++ src/com/lumos/Player.java
@@ -1,95 +1,15 @@
 package com.lumos;
 
-public class Player implements Character {
-	private int health;
-	private String name;
-	
-	private int strength;
-	
-	private int x;
-	private int y;
-	private int dx = 0;
-	private int dy = 0;
+public class Player extends Character {
 	
 	private Inventory inventory;
 	
 	public Player(int health, String name) {
-		this.health = health;
-		this.name = name;
+		super(health, name);
 		
 		inventory = new Inventory();
 
 		Item waterBottles = new Item(5, "Water Bottle", false, ItemType.CONSUMABLE);
 		inventory.addItem(waterBottles);
-	}
-	
-	@Override
-	public int getHealth() {
-		return health;
-	}
-	
-	@Override
-	public void setHealth(int health) {
-		this.health = health;
-	}
-
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public void setName(String name) {
-		this.name = name;		
-	}
-	
-	public int getStrength() {
-		return strength;
-	}
-	
-	public void setStrength(int strength) {
-		this.strength = strength;
-	}
-	
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	public int getX() {
-		return x;
-	}
-	
-	public void setY(int y) {
-		this.y = y;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public int getDx() {
-		return dx;
-	}
-	
-	public void setDx(int dx) {
-		this.dx = dx;
-	}
-	
-	public int getDy() {
-		return dy;
-	}
-	
-	public void setDy(int dy) {
-		this.dy = dy;
-	}
-	
-	public void moveX() {
-		x += dx;
-		dx = 0;
-	}
-	
-	public void moveY() {
-		y += dy;
-		dy = 0;
 	}
 }
