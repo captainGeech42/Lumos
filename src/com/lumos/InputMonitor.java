37d1a99692b7182f6aa4249f0d5f13b873aaac17
Zander Work <zander@zanderwork.com>
2017-03-15 10:16:06 -0700

    moved key listen code to keyPressed to allow repeat movement with a single keypress

--- src/com/lumos/InputMonitor.java
+++ src/com/lumos/InputMonitor.java
@@ -15,32 +15,32 @@ public class InputMonitor implements KeyListener {
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		//intentionally empty
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
 		Key key = Key.INVALID;
 		switch (e.getKeyCode()) {
-		case 37:
-			key = Key.LEFT_ARROW;
-			break;
-		case 39:
-			key = Key.RIGHT_ARROW;
-			break;
-		case 38:
-			key = Key.UP_ARROW;
-			break;
-		case 40:
-			key = Key.DOWN_ARROW;
-			break;
-		default:
-			break;
+			case 37:
+				key = Key.LEFT_ARROW;
+				break;
+			case 39:
+				key = Key.RIGHT_ARROW;
+				break;
+			case 38:
+				key = Key.UP_ARROW;
+				break;
+			case 40:
+				key = Key.DOWN_ARROW;
+				break;
+			default:
+				break;
 		}
 		if (log) {
 			System.out.println(String.format("received %s from %s", key, window));
 		}
 		window.processInput(key);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//intentionally empty
 	}
 
 	@Override
