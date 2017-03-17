cabb780dda13372df527915071fb7bc17e71978c
Zander Work <zander@zanderwork.com>
2017-03-17 10:01:08 -0700

    added a second item to test inventory scroll (it works)

--- src/com/lumos/Player.java
+++ src/com/lumos/Player.java
@@ -10,7 +10,9 @@ public class Player extends Character {
 		inventory = new Inventory();
 
 		Item waterBottles = new Item(5, "Water Bottle", false, ItemType.CONSUMABLE);
+		Item armor = new Item(8, "Cloth Armor", false, ItemType.ARMOR);
 		inventory.addItem(waterBottles);
+		inventory.addItem(armor);
 	}
 
 	public Inventory getInventory() {
